145a96398de7f149755a5eb65f7bc35bad94e08a
Davi <gonca.davi@gmail.com>
2025-08-20 21:10:13 -0300

    API Update

---
+++ API.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+from Clas import Clas
+
+API = Flask(__name__)
+
+example = Clas()
+
+@API.route('/text',methods=['GET'])
+def returnAll():
+    example.setTokens()
+    example.setWordFrequency()
+    example.setPerplexity()
+    example.calculateBurstiness()
+
+    return example.getAll()
+
+@API.route('/test',methods=['GET'])
+def returnTest():
+    return example.getText()
+
+# @API.route('/tokens_from_text',methods=['GET'])
+# def get_tokens_from_text():
+#     return s.get_tokens_from_text()
+
+# @API.route('/word_frequency',methods=['GET'])
+# def get_word_frequency():
+#     return s.get_word_frequency()
+
+# @API.route('/filter_punctuation',methods=['GET'])
+# def filter_punctuation():
+#     return s.filter_punctuation()
+
+# @API.route('/get_perplexity',methods=['GET'])
+# def perplexity():
+#     return s.perplexity()
+
+# @API.route('/calculate_burstiness',methods=['GET'])
+# def calculate_burstiness():
+#     return s.calculate_burstiness()
+
+@API.route('/text',methods=['POST'])
+def post_method():
+    new_text = request.get_data()
+    example.setText(new_text)
+    return example.getText()
+
+API.run(port=500, host='localhost', debug=True)
---
+++ Clas.py
@@ -0,0 +1,34 @@
+import Start as s
+import json
+
+class Clas:
+    rawText = ""
+    tokens = []
+    word_frequency = ""
+    perplexity = 0
+    burstiness_scores = ""
+
+    def setText(self, text):
+        self.rawText = text
+
+    def getText(self):
+        return self.rawText
+    
+    def setTokens(self):
+        self.tokens = s.get_tokens_from_text(self.rawText)
+
+    def setWordFrequency(self):
+        self.word_frequency = s.get_word_frequency(self.tokens)
+
+    def setPerplexity(self):
+        self.perplexity = s.get_perplexity(self.rawText)
+
+    def calculateBurstiness(self):
+        self.burstiness_scores = s.calculate_burstiness(self.tokens)
+
+    def getAll(self):
+        dictionary = {"Text": self.rawText, "tokens": self.tokens,
+                      "word_frequency": self.word_frequency, "perplexity": self.perplexity,
+                      "burstiness_scores": self.burstiness_scores}
+        textInJson = json.dumps(dictionary)
+        return textInJson
